0305780a91b085c0c5fd6df35fc5ac2305b81d01
ky5133 <dinshika1986@gmail.com>
2026-07-16 19:52:56 +0400

    commit

---
+++ class 1/class 8/homework.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image = cv2.imread("C:/Users/Dinesh Bachani/Downloads/download.jpg")
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+(w,h)=image.shape[:2]
+center = (w//2, h//2)
+M=cv2.getRotationMatrix2D(center,55,1.0)
+rotated=cv2.warpAffine(image,M,(w,h))
+rotated_rgb = cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB)
+plt.imshow(rotated_rgb)
+plt.title("Rotated Image")
+plt.show()
+brightness_matrix=np.ones(image.shape,dtype="uint8")*200
+brighter=cv2.add(image,brightness_matrix)
+brighter_rgb=cv2.cvtColor(brighter,cv2.COLOR_BGR2RGB)
+plt.imshow(brighter_rgb)
+plt.title("Brighter RGB")
+plt.show()
+cropped_image=image[100:120,100:120]
+cropped_rgb = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
+plt.imshow(cropped_rgb)
+plt.title("Cropped Region")
+plt.show()
